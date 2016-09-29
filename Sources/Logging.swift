9ee4e3f41cfd1a4b51a2df458b4080695a5394fe
Jaanus Siim <jaanus@jaanussiim.com>
2016-09-29 10:26:49 +0300

    Logger change

--- Sources/Logging.swift
+++ Sources/Logging.swift
@@ -16,19 +16,19 @@
 
 import Foundation
 
-public protocol LoggingDelegate: class {
+public protocol Logger {
     func log<T>(_ object: T, file: String, function: String, line: Int)
 }
 
 public class Logging {
-    public weak var delegate: LoggingDelegate?
+    private var logger: Logger?
     
     private static let sharedInstance = Logging()
-    public static func setDelegate(_ delegate: LoggingDelegate) {
-        sharedInstance.delegate = delegate
+    public static func set(logger: Logger) {
+        sharedInstance.logger = logger
     }
     
     internal class func log<T>(_ object: T, file: String = #file, function: String = #function, line: Int = #line) {
-        sharedInstance.delegate?.log(object, file: file, function: function, line: line)
+        sharedInstance.logger?.log(object, file: file, function: function, line: line)
     }
 }
