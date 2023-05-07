b728007f23cef1b0a5502251d19481fd5efee329
Jaanus Siim <jaanus@jaanussiim.com>
2023-05-07 09:45:32 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["TalkToCloud"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.3"),
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "0.3.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "0.10.2")
     ],
     targets: [
         .target(
