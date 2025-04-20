64af15cbcbaaf53391490e5161250d3b08572613
Jaanus Siim <jaanus@jaanussiim.com>
2025-04-20 07:35:06 +0300

    Networking

--- Sources/TalkToCloud/AsyncSystemFetch.swift
+++ Sources/TalkToCloud/AsyncSystemFetch.swift
@@ -15,6 +15,9 @@
  */
 
 import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
 
 extension NetworkFetch {
   public static let asyncSystemFetch = NetworkFetch(
--- Sources/TalkToCloud/Authentication.swift
+++ Sources/TalkToCloud/Authentication.swift
@@ -15,6 +15,9 @@
  */
 
 import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
 
 public struct Authentication {
   private let onParams: (() -> [String: String])
